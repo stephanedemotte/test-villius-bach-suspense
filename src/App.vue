2fb950fe96d826447e107fb44de514d42235a074
Stephane Demotte <stephane@demotte.net>
2020-10-09 08:01:23 -0400

    big bang

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Suspense>
+    <HelloWorld />
+  </Suspense>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import { useClient, fetch } from 'villus'
+import { batch } from '@villus/batch'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup() {
+    useClient({
+      url: "https://pet-library.moonhighway.com/",
+      use: [batch({ timeout: 1000 })]
+    });
+  },
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  {{ data }}
+</template>
+
+<script>
+import { useQuery } from 'villus'
+export default {
+  name: 'HelloWorld',
+  async setup() {
+    const { data } = await useQuery(`
+      query AllPets ($category: PetCategory) {
+        allPets (category: $category) {
+          id
+          name
+          category
+          photo {
+            thumb
+            full
+          }
+        }
+      }
+    `)
+    return { data }
+  }
+}
+</script>
